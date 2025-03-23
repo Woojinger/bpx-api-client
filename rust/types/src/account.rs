378dddbc84434805a6cda37398037d2096e0b070
신우진 <48384542+Woojinger@users.noreply.github.com>
2025-03-23 21:38:14 +0900

    skpip

--- rust/types/src/account.rs
+++ rust/types/src/account.rs
@@ -5,8 +5,11 @@ use crate::order::{LimitOrder, MarketOrder};
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct PatchAccountPayload {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub autoBorrowSettlements: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub autoLend: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub autoRepayBorrows: Option<bool>,
     pub leverageLimit: Option<Decimal>,
 }
