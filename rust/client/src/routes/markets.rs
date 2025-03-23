f3660a72a106ce645e77845557133bd09ca0a24f
신우진 <48384542+Woojinger@users.noreply.github.com>
2025-03-23 19:57:34 +0900

    Update markets.rs
    Signed-off-by: 신우진 <48384542+Woojinger@users.noreply.github.com>

--- rust/client/src/routes/markets.rs
+++ rust/client/src/routes/markets.rs
@@ -43,7 +43,7 @@ impl BpxClient {
 
     /// Retrieves the order book depth for a given symbol.
     pub async fn get_order_book_depth(&self, symbol: &str) -> Result<OrderBookDepth> {
-        let url = format!("{}{}&symbol={}", self.base_url, API_DEPTH, symbol);
+        let url = format!("{}{}?symbol={}", self.base_url, API_DEPTH, symbol);
         let res = self.get(url).await?;
         res.json().await.map_err(Into::into)
     }
