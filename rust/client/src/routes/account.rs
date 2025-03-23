ac1b1f7c7a61f0fb31ca81cba5541a8c5b1fb702
신우진 <48384542+Woojinger@users.noreply.github.com>
2025-03-23 21:46:49 +0900

    patch

--- rust/client/src/routes/account.rs
+++ rust/client/src/routes/account.rs
@@ -17,6 +17,20 @@ impl BpxClient {
     pub async fn patch_account(&self, paylaod: PatchAccountPayload) -> Result<AccountDetails, Box<dyn Error>> {
         let endpoint = format!("{}{}", self.base_url, API_ACCOUNT);
         let res = self.patch(endpoint, paylaod).await?;
-        res.json().await.map_err(Into::into)
+        // Check if the response is successful and not empty
+        if res.status().is_success() {
+            if res.status().as_u16() == 204 {
+                // Handle 204 No Content specifically
+                Err("No Content to update".into())
+            } else {
+                match res.json::<AccountDetails>().await {
+                    Ok(account_details) => Ok(account_details),
+                    Err(e) => Err(e.into()),
+                }
+            }
+        } else {
+            // You can handle different status codes here if needed
+            Err(format!("Failed with status: {}", res.status()).into())
+        }
     }
 }
